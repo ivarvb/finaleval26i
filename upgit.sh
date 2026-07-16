45177c59a5b2c61135ea59dc6e00b6ff0126eda2
l.ivarvb <l.ivarvb@github.com>
2026-07-16 07:30:09 -0500

    Initial commit

---
+++ upgit.sh
@@ -0,0 +1,9 @@
+rm -rf .git
+git init
+git config --local user.name "l.ivarvb"
+git config --local user.email "l.ivarvb@github.com"
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/ivarvb/finaleval26i
+git push -u --force origin master
+
